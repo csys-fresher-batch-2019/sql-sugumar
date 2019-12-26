5b069256cb044df79370c3bb2f3258f78127c531
sugumar <sugumar1220@gmail.com>
2019-12-26 16:31:27 +0530

    Update task_1.sql

--- task_1.sql
+++ task_1.sql
@@ -1,9 +1,9 @@
 create table task_1 (user_id number(10),task_name varchar2(20),
 gmail_id varchar2(20),password varchar2(10),salary number(30));
-insert into task_1(user_id,task_name,gmail_id,password,salary)values(1,'a','suri@gmail.com','1234',2000);
-insert into task_1(user_id,task_name,gmail_id,password,salary)values(2,'b','s@gmail.com','234',1000);
-insert into task_1(user_id,task_name,gmail_id,password,salary)values(3,'d','si@gmal.com','1234',9000);
-insert into task_1(user_id,task_name,gmail_id,password,salary)values(4,'d','s@gmail.com','234',24000);
-insert into task_1(user_id,task_name,gmail_id,password,salary)values(5,'e','sri@gmal.com','1234',2000);
+insert into task_1(user_id,task_name,gmail_id,password,salary)values(1,'java','suri@gmail.com','1234',2000);
+insert into task_1(user_id,task_name,gmail_id,password,salary)values(2,'oracle','s@gmail.com','234',1000);
+insert into task_1(user_id,task_name,gmail_id,password,salary)values(3,'oracle','si@gmal.com','1234',9000);
+insert into task_1(user_id,task_name,gmail_id,password,salary)values(4,'postgres','s@gmail.com','234',24000);
+insert into task_1(user_id,task_name,gmail_id,password,salary)values(5,'mysql','sri@gmal.com','1234',2000);
 
 select * from task_1;
